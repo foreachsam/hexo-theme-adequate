7470e60e35e6eb39160ec8e67bbcbf89daa55f78
foreachsam <foreachsam@users.noreply.github.com>
2016-02-27 00:23:05 +0800

    scroll to top btn hide

--- source/asset/lib/sys.js
+++ source/asset/lib/sys.js
@@ -2,14 +2,31 @@
 !function(global, $) {
 	var ui = {
 		scrollToTop: function(){
-			$('body').velocity('scroll', {duration: 500, easing: 'spring'}); //http://julian.com/research/velocity/#scroll
+			$('body').velocity('scroll', {duration: 1000, easing: 'spring'}); //http://julian.com/research/velocity/#scroll
 		}
 	};
 
-	$(document).ready(function(){
-		$(document).on('click', '.ui-btn-top', function(evt) {
+	$(document).ready(function() {
+
+		var btnTop = '.ui-btn-top';
+		var btnTopHide = '.ui-btn-top-hide';
+
+		$(document).on('click', btnTop, function(evt) {
 			ui.scrollToTop();
 		});
+
+		$(document).on('click', btnTopHide, function(evt) {
+			ui.scrollToTop();
+		});
+
+		$(window).on('scroll', function(){
+			if ($(this).scrollTop() > 100) {
+				$(btnTopHide).fadeIn();
+			} else {
+				$(btnTopHide).fadeOut();
+			}
+		});
+
 	});
 	global.ui = ui;
 }(window, jQuery);
